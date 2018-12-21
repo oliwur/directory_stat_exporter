9832f5959d55c1b45b37d416c8fe234bc530c775
Oli <codestoke@gmail.com>
2018-12-21 21:15:32 +0100

    refactored - extracted dir counting functions to new file

--- dirstat.go
+++ dirstat.go
@@ -141,42 +141,6 @@ func getOldestAgeInDir(dir string) int64 {
 	return oldestTs
 }
 
-func getFileCount(dir string, recursive bool) int {
-	if recursive {
-		return getFileCountInDirRecursively(dir)
-	} else {
-		return getFileCountInDir(dir)
-	}
-}
-
-func getFileCountInDirRecursively(dir string) int {
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		return -1
-	}
-	count := 0
-	_ = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			count++
-		}
-		return nil
-	})
-	return count
-}
-
-func getFileCountInDir(dir string) int {
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return -1
-	}
-	count := 0
-	for _, f := range files {
-		if !f.IsDir() {
-			count++
-		}
-	}
-	return count
-}
-
 // this should be replaced with one more generic generator.
 func sprintCurrentTimestamp(m metric) string {
 	str := ""
---
+++ dirstatCounter.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func getFileCount(dir string, recursive bool) int {
+	if recursive {
+		return getFileCountInDirRecursively(dir)
+	} else {
+		return getFileCountInDir(dir)
+	}
+}
+
+func getFileCountInDirRecursively(dir string) int {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return -1
+	}
+	count := 0
+	_ = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			count++
+		}
+		return nil
+	})
+	return count
+}
+
+func getFileCountInDir(dir string) int {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return -1
+	}
+	count := 0
+	for _, f := range files {
+		if !f.IsDir() {
+			count++
+		}
+	}
+	return count
+}
